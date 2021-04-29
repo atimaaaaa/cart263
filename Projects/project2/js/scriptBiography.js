90677d98554d99171f20de00304751bfd4ad3de8
Atima <atima.ng@hotmail.ca>
2021-04-29 15:57:00 -0400

    P2: Added responsive voice library

--- Projects/project2/js/scriptBiography.js
+++ Projects/project2/js/scriptBiography.js
@@ -14,6 +14,7 @@ AUDIO CREDITS
 Recorder audio: https://freesound.org/people/dela.deb/sounds/463725/
 Ukulele strumming audio: https://freesound.org/people/melismw12/sounds/510922/
 Lopp Pi Dee Loop audio: https://freesound.org/people/visual/sounds/338948/
+Tin Man audio: https://freesound.org/people/wikbeats/sounds/211869/
 
 */
 "use strict";
@@ -37,8 +38,30 @@ $(function() {
     placeholder: "ui-state-highlight",
     cursor: "grabbing",
     distance: 40,
-    start: function(event, ui) {},
-    stop: function(event, ui) {}
+    //When element is being sorted, plays the song and pauses other songs if they are playing
+    start: function(event, ui) {
+      if (ui.item.hasClass("audio1")) {
+        audio1.play();
+        audio2.pause();
+        audio3.pause();
+        audio4.pause();
+      } else if (ui.item.hasClass("audio2")) {
+        audio2.play();
+        audio1.pause();
+        audio3.pause();
+        audio4.pause();
+      } else if (ui.item.hasClass("audio3")) {
+        audio3.play();
+        audio1.pause();
+        audio2.pause();
+        audio4.pause();
+      } else if (ui.item.hasClass("audio4")) {
+        audio4.play();
+        audio1.pause();
+        audio2.pause();
+        audio3.pause();
+      }
+    }
   });
   $("#sortable").disableSelection();
 });
