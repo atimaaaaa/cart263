2bb7d05bcf83144d2ae66242ac87afe4eb7d8bb9
Atima <atima.ng@hotmail.ca>
2021-04-28 22:53:34 -0400

    P2: Added random color tiles

--- Projects/project2/js/scriptDance.js
+++ Projects/project2/js/scriptDance.js
@@ -1,5 +1,5 @@
 /*
-DJ TERRIBLE PORTFOLIO
+DJ TERRIBLE PORTFOLIO - DANCE
 Atima Ng
 
 Uses:
@@ -9,7 +9,47 @@ https://jquery.com
 jQuery UI:
 https://jqueryui.com
 
+Audio: https://freesound.org/people/stankbeast/sounds/342360/
+
 
 */
 
 "use strict";
+
+const colorBtn = document.querySelector("#btn-1");
+const boxes = document.querySelectorAll(".box");
+
+let playing = false;
+
+//Get random color hex code
+function randomColor() {
+  let letters = "0123456789ABCEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+//To add color to square boxes
+function startRandomColor() {
+  if (playing) {
+    boxes.forEach(color => {
+      color.style.background = randomColor();
+    });
+    //Changes color every 500ms
+    setTimeout(startRandomColor, 500);
+  }
+}
+
+//Changes color of the "dance floor" on click
+colorBtn.addEventListener("click", function() {
+  colorBtn.innerText = "TURN IT DOWN...";
+  if (playing) {
+    playing = false;
+    colorBtn.innerText = "TURN IT UP!!!";
+  } else {
+    playing = true;
+    startRandomColor();
+  }
+});
